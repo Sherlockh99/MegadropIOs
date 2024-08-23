79db69881923ac905cc148499a67a2792cfa85e8
Sherlock <sherlockh989@gmail.com>
2024-08-24 02:46:34 +0300

    Rename OrdersFileXDTO to FileDataXDTO

--- Megadrop/Model/XDTO/FileDataXDTO.swift
+++ Megadrop/Model/XDTO/FileDataXDTO.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-struct OrdersFileXDTO: Codable, Hashable{
+struct FileDataXDTO: Codable, Hashable{
     let FileName: String
     let FileData: String
 }
---
+++ Megadrop/Views/FileShareViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FileShareViewController.swift
+//  Megadrop
+//
+//  Created by Андрій Коробчук on 24.08.2024.
+//
+
+import Foundation
