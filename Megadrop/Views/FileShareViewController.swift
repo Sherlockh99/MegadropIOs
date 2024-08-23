caacdd2e570e3fa5cd5df00a5bc7065473252c8f
Sherlock <sherlockh989@gmail.com>
2024-08-24 02:48:45 +0300

    Added FileShareViewController

--- Megadrop/Views/FileShareViewController.swift
+++ Megadrop/Views/FileShareViewController.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import UIKit
+
+class FileShareViewController: UIViewController {
+    var fileURL: URL?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let fileURL = fileURL {
+            let documentInteractionController = UIDocumentInteractionController(url: fileURL)
+            documentInteractionController.delegate = self
+            documentInteractionController.presentOptionsMenu(from: self.view.frame, in: self.view, animated: true)
+        }
+    }
+}
+
+extension FileShareViewController: UIDocumentInteractionControllerDelegate {
+    // Реализуйте методы делегата, если нужно
+}
